3dbac76b1d1b12b03f7a4ea49259b5d9135a9d31
Kevanana26 <525kpiccinich@frhsd.com>
2024-12-12 11:04:07 -0500

    Conditional rendering done

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product:'Socks'
-            image: './assets/images/sock_green.jpg'
+            product:'Socks',
+            image:'assets/images/socks_green.jpg',
+            inventory: 169
         }
     }
 })
