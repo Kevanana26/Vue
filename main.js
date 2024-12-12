0dd4e1723b488c1116a3dfa024ff2c5501e54015
Kevanana26 <525kpiccinich@frhsd.com>
2024-12-12 11:17:12 -0500

    List rendering done

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const app = Vue.createApp({
             product:'Socks',
             image:'assets/images/socks_green.jpg',
             inventory: 169
+            details: ['50% Cotton', '30% Wool', '20% Polyester']
+            variants:[
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ]
         }
     }
 })
